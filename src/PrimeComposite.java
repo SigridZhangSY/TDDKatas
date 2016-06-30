81ddaef34047f91d694f9b2a8f26ded56af87633
SigridZhangSY <sigridzhang0603@163.com>
2016-06-30 20:28:25 +0800

    Update PrimeComposite.java

--- src/PrimeComposite.java
+++ src/PrimeComposite.java
@@ -1,43 +1,76 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
-public class PrimeComposite {
-	private boolean isPrime(int num){
-		if(num == 1)
-			return false;
-		if(num == 2)
-			return true;
-		for(int i = 2; i < num; i++){
-			if(num%i == 0)
-				return false;
-		}
-		return true;
-	}
+public class StringCalculator {
 	
-	private boolean isEven(int num){
-		if(num%2 == 0)
-			return true;
-		else
-			return false;
-	}
-	
-	public String printNumber(int num){
-		if(isPrime(num)){
-			return "prime";
-		}
-		else{
-			if(num != 1 && !isEven(num))
-				return "composite";
+	private int addSum(String[] numbers) throws Exception{
+		int sum = 0;
+		StringBuilder negativeNum = new StringBuilder();
+		for(String num : numbers){
+			if(Integer.valueOf(num) >= 0){
+				if(Integer.valueOf(num) <= 1000 )
+					sum += Integer.valueOf(num);
+			}
 			else
-				return String.valueOf(num);
+				negativeNum.append(num + ",");
 		}
+		if(negativeNum.length() > 0)
+			throw new Exception("negatives not allowed - " + negativeNum.substring(0, negativeNum.length()-1));
+		
+		return sum;
 	}
 	
-	public String printRes(int n1, int n2) throws Exception{
-		if(n1 <= 0 || n2 > 100 || n1 > n2)
-			throw new Exception("INVALID PARAMETER");
-		StringBuilder res = new StringBuilder();
-		for(int i = n1; i <= n2; i++){
-			res.append(printNumber(i) + " ");
-		}
-		return res.substring(0, res.length()-1);
+	private boolean isSpecified(String str){
+		if(str.charAt(0) >= '0' && str.charAt(0) <= '9')
+			return false;
+		else
+			return true;
+			
 	}
+	
+	private String getSpecified(String delimiterStr){
+		if(delimiterStr.length() == 1)
+			return delimiterStr;
+		else{
+			String regex = "(?<=\\[)([^\\]])*?(?=\\])";
+			Pattern p = Pattern.compile(regex);
+			Matcher m = p.matcher(delimiterStr);
+			StringBuilder res = new StringBuilder();
+			while(m.find()){
+				String regxRes = new String();
+				regxRes = m.group();
+				//replace "\" first
+				regxRes = regxRes.replaceAll("\\\\", "\\\\\\+");
+				regxRes = regxRes.replaceAll("\\*", "\\\\*");
+				regxRes = regxRes.replaceAll("\\+", "\\\\+");
+				regxRes = regxRes.replaceAll("\\|", "\\\\|");
+				res.append(regxRes + "|");
+
+			}
+			if(res.length() > 0)
+				return res.substring(0, res.length()-1);
+			else
+				return null;
+		}
+			
+	}
+	
+	public int getRes(String str) throws Exception{
+		String delimiters;
+		String[] numbers;
+		if(str == "" || str == null)
+			return 0;
+		if(isSpecified(str)){
+			String[] resSplit = str.split("\n");
+			delimiters = getSpecified(resSplit[0]);
+			numbers = resSplit[1].split(delimiters);
+		}
+		else
+			 numbers = str.split(",|\n");
+		
+		return addSum(numbers);
+	}
+	
+	
+
 }
